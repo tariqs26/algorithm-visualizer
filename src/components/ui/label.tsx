df62c202f43d827502023b9a8c3976cdff25fd80
Saad Tariq <saadtariq.dev@outlook.com>
2024-09-09 23:11:46 -0400

    fix: label component prop type

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils"
 export const Label = ({
   className,
   ...props
-}: React.HTMLAttributes<HTMLLabelElement>) => (
+}: React.ComponentPropsWithoutRef<"label">) => (
   <label
     className={cn(
       "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
