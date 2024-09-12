7e681baa66cf5b0a9a6914c4f483cb0bfd36d417
Saad Tariq <saadtariq.dev@outlook.com>
2024-09-11 22:17:35 -0400

    refactor: wrap component props type with readonly

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils"
 export const Label = ({
   className,
   ...props
-}: React.ComponentPropsWithoutRef<"label">) => (
+}: Readonly<React.ComponentPropsWithoutRef<"label">>) => (
   <label
     className={cn(
       "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
