153e5323df89ab56ccb9b36fc6f85cbce5b73fec
Saad Tariq <saadtariq.dev@outlook.com>
2024-09-15 17:59:34 -0400

    feat: add `siteConfig` for global site metadata

---
+++ src/config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  title: "Algorithm Visualizer",
+  description: "Visualize popular pathfinding and sorting algorithms.",
+  url: "https://algorithm-visualizer-dev.vercel.app",
+}
