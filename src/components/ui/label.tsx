d14483cf6dd99d61d34a3d6f52aa7e67bc3e847f
Saad Tariq <saadtariq.dev@outlook.com>
2025-01-25 16:52:42 -0500

    refactor: update Label component prop type to use `ComponentPropsWithRef`

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils"
 export const Label = ({
   className,
   ...props
-}: Readonly<React.ComponentPropsWithoutRef<"label">>) => (
+}: Readonly<React.ComponentPropsWithRef<"label">>) => (
   <label
     className={cn(
       "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
